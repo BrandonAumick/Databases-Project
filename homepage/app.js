7e8ea4d42be3494d6afc20ddc183b73dbea163b3
Brandon Aumick <32585448+BrandonAumick@users.noreply.github.com>
2023-04-22 18:21:55 -0400

    Update app.js

--- homepage/app.js
+++ homepage/app.js
@@ -1,16 +1,1 @@
-susButton = document.getElementById('susButton');
-theftButton = document.getElementById('theftButton');
-vehicleButton = document.getElementById('vehicleButton');
-headThing = document.querySelector('h1');
 
-susButton.addEventListener('click', () => {
-    headThing.textContent += 'sus'
-})
-
-theftButton.addEventListener('click', () => {
-    headThing.textContent += 'theft'
-})
-
-vehicleButton.addEventListener('click', () => {
-    headThing.textContent += 'veh'
-})
