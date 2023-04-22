1ac9a266c713a8e5b4ecc913c1eb0be00cd6a221
Brandon Aumick <32585448+BrandonAumick@users.noreply.github.com>
2023-04-22 15:20:20 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const app = express();
 const hbs = require('hbs');
+const mysql = require('mysql');
+const util = require('util');
+
+
+function makeDb( config ) {
+
+    const connection = mysql.createConnection( config );  return {
+      query( sql, args ) {
+        return util.promisify( connection.query )
+          .call( connection, sql, args );
+      },
+      close() {
+        return util.promisify( connection.end ).call( connection );
+      }
+    };
+
+  }
 
 
 async function main() {
@@ -16,7 +33,7 @@ async function main() {
 
     app.get('/susPage', (req, res) => {
         res.render("susPage", {
-            susStuff: '<h1>You are sus</h1>'
+            susStuff: ''
         });
     });
 
@@ -32,8 +49,44 @@ async function main() {
         });
     });
 
+    app.post('/susPage', async (req, res) => {
+
+        let db = makeDb({
+            host: "localhost",
+            user: "root",
+            password: "&r!Xfy%te7uD#3UZ6S&C"
+        });
+
+        await db.query('USE `the-offenders`;');
+
+        let attributes = {person_ID: '^*', address: '^*', bDay: '^*', fName: '^*', lName: '^*', mInt: '^*', dlNum: '^*'};
+
+        for (id in attributes) {
+            if (req.body[id]) {
+                attributes[id] = req.body[id];
+            }
+        }
+
+        console.log(attributes);
+
+        let result = await db.query(`SELECT * FROM subject WHERE person_ID REGEXP "${attributes['person_ID']}" AND address REGEXP "${attributes['address']}" 
+                                    AND fName REGEXP "${attributes['fName']}" AND lName REGEXP "${attributes['lName']}" 
+                                    AND mInt REGEXP "${attributes['mInt']}" AND dlNum REGEXP "${attributes['dlNum']}";`)
+
+        let sendString = "";
+
+        for (person of result) {
+            sendString += `| ${person['person_ID']} | ${person['address']} | ${person['bDay']} | ${person['fName']} | ${person['lName']} | ${person['mInt']} | ${person['dlNum']} |\n`;
+        }
+
+        res.render("susPage", {
+            susStuff: sendString
+        });
+
+    });
+
 }
 
 main();
 
-// Start the web server
+
