8225ccf769239ef1d60aafa16a96db3fe307d71e
Brandon Aumick <32585448+BrandonAumick@users.noreply.github.com>
2023-04-25 13:38:28 -0400

    Update app.js

--- views/app.js
+++ views/app.js
@@ -1,16 +1,7 @@
-const searchButton = document.getElementById('searchButton');
-const tableDiv = document.getElementById('tableDiv');
-const nameInput = document.getElementById('name_input');
-
-
-
-/*searchButton.addEventListener('click', printResults(nameInput.value));
-
-
-
-  function printResults(searchValue) {
-
-    tableDiv.innerHTML = `${searchValue} is kinda sus`;
-
-
-  }*/
+searchStuff = document.querySelector('div');
+console.log(searchStuff.innerHTML)
+const re = /.*/g
+oldStuff = searchStuff.innerHTML.match(re);
+newString = searchStuff.innerHTML.replaceAll(re, oldStuff[0]);
+console.log(newString);
+searchStuff.innerHTML = newString;
