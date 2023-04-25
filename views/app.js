5cfcd772a0cce78e26a380ade97fe69b90561708
Brandon Aumick <32585448+BrandonAumick@users.noreply.github.com>
2023-04-25 13:39:42 -0400

    Delete app.js

--- views/app.js
+++
@@ -1,7 +0,0 @@
-searchStuff = document.querySelector('div');
-console.log(searchStuff.innerHTML)
-const re = /.*/g
-oldStuff = searchStuff.innerHTML.match(re);
-newString = searchStuff.innerHTML.replaceAll(re, oldStuff[0]);
-console.log(newString);
-searchStuff.innerHTML = newString;
